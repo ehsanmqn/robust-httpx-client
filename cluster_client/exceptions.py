f7bece248e798dd09413199fd47928cb6bf914da
Ehsan <spin313@gmail.com>
2024-07-18 19:37:58 +0330

    ServerErrorException and ThrottleErrorException added

--- cluster_client/exceptions.py
+++ cluster_client/exceptions.py
@@ -6,3 +6,25 @@ class GroupOperationException(Exception):
     def __init__(self, message):
         self.message = message
         super().__init__(self.message)
+
+
+class ServerErrorException(Exception):
+    """
+    Custom exception for server errors (HTTP 500-599).
+    """
+
+    def __init__(self, host, status_code):
+        message = f'Server error on {host}: {status_code}'
+        self.message = message
+        super().__init__(self.message)
+
+
+class ThrottleErrorException(Exception):
+    """
+    Custom exception for throttling errors (HTTP 429).
+    """
+
+    def __init__(self, host, status_code):
+        message = f'Throttling error on {host}: {status_code}'
+        self.message = message
+        super().__init__(self.message)
