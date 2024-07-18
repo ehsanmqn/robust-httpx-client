821d3846fc817aec28a8f71e35a6f17448a48e79
Ehsan <spin313@gmail.com>
2024-07-18 16:03:59 +0330

    GroupOperationException implemented

--- cluster_client/exceptions.py
+++ cluster_client/exceptions.py
@@ -1,7 +1,8 @@
-
 class GroupOperationException(Exception):
     """
     Custom exception for group operation failures.
-
     """
-    pass
+
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
