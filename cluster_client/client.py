4ee7968cbd7dfa3ac1789aef897400d27166e533
Ehsan <spin313@gmail.com>
2024-07-18 12:52:31 +0330

    _delete_group_on_host has been implemented

--- cluster_client/client.py
+++ cluster_client/client.py
@@ -27,3 +27,24 @@ class ClusterClient:
             logger.error(f'Request error occurred: {exc}')
 
         return False
+
+    async def _delete_group_on_host(self, client: httpx.AsyncClient, host: str, group_id: str) -> bool:
+        """
+        Delete a group from a specific host.
+        """
+
+        url = f'{host}/v1/group/'
+        try:
+            # httpx allows sending json parameters only through the request method not the delete method since it's discouraged
+            # Ref: https://github.com/encode/httpx/discussions/1587
+            response = await client.request(method='DELETE', url=url, json={'groupId': group_id})
+            if response.status_code == 200:
+                logger.info(f'Group {group_id} deleted from {host}')
+                return True
+
+            logger.error(f'Failed to delete group on {host}: {response.status_code}')
+
+        except httpx.RequestError as exc:
+            logger.error(f'Request error occurred: {exc}')
+
+        return False
