8f54229c3b695289d6d06ebdd47d36acc945dd17
Ehsan <spin313@gmail.com>
2024-07-18 13:56:47 +0330

    delete_group and create_group methods have been implemented

--- cluster_client/client.py
+++ cluster_client/client.py
@@ -1,6 +1,7 @@
 import httpx
 import logging
 from .config import HOSTS
+from .exceptions import GroupOperationException
 
 logger = logging.getLogger(__name__)
 
@@ -48,3 +49,44 @@ class ClusterClient:
             logger.error(f'Request error occurred: {exc}')
 
         return False
+
+    async def _rollback_creation(self, client: httpx.AsyncClient, group_id: str, success_hosts: list) -> None:
+        """
+        Rollback group creation on hosts where it was successfully created.
+        """
+
+        logger.info('Rolling back creation on successful hosts...')
+
+        for host in success_hosts:
+            await self._delete_group_on_host(client, host, group_id)
+
+    async def create_group(self, group_id: str) -> bool:
+        """
+        Create a group on all cluster nodes. Rolls back if any creation fails.
+        """
+
+        async with httpx.AsyncClient() as client:
+            success_hosts = []
+            try:
+                for host in self.hosts:
+                    if not await self._create_group_on_host(client, host, group_id):
+                        raise GroupOperationException(f'Failed to create group on {host}, initiating rollback.')
+                    success_hosts.append(host)
+
+            except GroupOperationException as e:
+                logger.error(f'Error during creation: {e}')
+                await self._rollback_creation(client, group_id, success_hosts)
+                return False
+
+            return True
+
+    async def delete_group(self, group_id: str) -> bool:
+        """
+        Delete a group from all cluster nodes.
+        """
+
+        async with httpx.AsyncClient() as client:
+            for host in self.hosts:
+                await self._delete_group_on_host(client, host, group_id)
+
+            return True
