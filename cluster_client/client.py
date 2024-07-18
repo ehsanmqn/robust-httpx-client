bc56448eb813ff6ae741cb8ca413b7df81fd131c
Ehsan <spin313@gmail.com>
2024-07-18 20:11:16 +0330

    Implementation improved

--- cluster_client/client.py
+++ cluster_client/client.py
@@ -3,7 +3,11 @@ import logging
 from tenacity import retry, stop_after_attempt, wait_exponential, retry_if_exception_type
 
 from .config import HOSTS
-from .exceptions import GroupOperationException, ServerErrorException, ThrottleErrorException
+from .exceptions import (
+    GroupOperationException,
+    HandleableErrorException,
+    RequestErrorException
+)
 
 logger = logging.getLogger(__name__)
 
@@ -12,45 +16,61 @@ class ClusterClient:
     def __init__(self, hosts=HOSTS):
         self.hosts = hosts
 
-    @retry(retry=(retry_if_exception_type(ServerErrorException) | retry_if_exception_type(ThrottleErrorException)), stop=stop_after_attempt(3), wait=wait_exponential(multiplier=1, min=1, max=10))
+    @retry(
+        retry=retry_if_exception_type(HandleableErrorException),
+        stop=stop_after_attempt(3),
+        wait=wait_exponential(multiplier=1, min=1, max=10)
+    )
     async def _create_group_on_host(self, client: httpx.AsyncClient, host: str, group_id: str) -> bool:
         """
         Create a group on a specific host.
         """
 
         url = f'{host}/v1/group/'
+
         try:
             response = await client.post(url, json={'groupId': group_id}, timeout=10.0)
             if response.status_code == 201:
                 logger.info(f'Group {group_id} created on {host}')
                 return True
-            logger.error(f'Failed to create group on {host}: {response.status_code}')
-        except httpx.TimeoutException:
-            logger.error(f'Timeout occurred while creating group on {host}')
+            elif response.status_code == 429 or 500 <= response.status_code < 600:
+                logger.warning(f'Error {response.status_code} while creating group on {host}')
+                raise HandleableErrorException(host, response.status_code)
+            else:
+                logger.error(f'Failed to create group on {host}: {response.status_code}')
+                return False
+
         except httpx.RequestError as exc:
             logger.error(f'Request error occurred while creating group on {host}: {exc}')
+            raise RequestErrorException(host, str(exc))
 
-        return False
-
-    @retry(retry=retry_if_exception_type(httpx.RequestError), stop=stop_after_attempt(3), wait=wait_exponential(multiplier=1, min=1, max=10))
+    @retry(
+        retry=retry_if_exception_type(HandleableErrorException),
+        stop=stop_after_attempt(3),
+        wait=wait_exponential(multiplier=1, min=1, max=10)
+    )
     async def _delete_group_on_host(self, client: httpx.AsyncClient, host: str, group_id: str) -> bool:
         """
         Delete a group from a specific host.
         """
 
         url = f'{host}/v1/group/'
+
         try:
-            # httpx allows sending json parameters only through the request method not the delete method since it's discouraged
-            # Ref: https://github.com/encode/httpx/discussions/1587
             response = await client.request(method='DELETE', url=url, json={'groupId': group_id}, timeout=10.0)
             if response.status_code == 200:
                 logger.info(f'Group {group_id} deleted from {host}')
                 return True
-            logger.error(f'Failed to delete group on {host}: {response.status_code}')
-        except httpx.RequestError as exc:
-            logger.error(f'Request error occurred: {exc}')
+            elif response.status_code == 429 or 500 <= response.status_code < 600:
+                logger.warning(f'Error {response.status_code} while deleting group on {host}')
+                raise HandleableErrorException(host, response.status_code)
+            else:
+                logger.error(f'Failed to delete group on {host}: {response.status_code}')
+                return False
 
-        return False
+        except httpx.RequestError as exc:
+            logger.error(f'Request error occurred while deleting group on {host}: {exc}')
+            raise RequestErrorException(host, str(exc))
 
     async def _rollback_creation(self, client: httpx.AsyncClient, group_id: str, success_hosts: list) -> None:
         """
@@ -58,6 +78,7 @@ class ClusterClient:
         """
 
         logger.info('Rolling back creation on successful hosts...')
+
         for host in success_hosts:
             if not await self._delete_group_on_host(client, host, group_id):
                 logger.error(f'Failed to rollback creation on {host}')
@@ -89,5 +110,4 @@ class ClusterClient:
         async with httpx.AsyncClient() as client:
             for host in self.hosts:
                 await self._delete_group_on_host(client, host, group_id)
-
             return True
