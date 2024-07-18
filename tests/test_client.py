7c170e0720887c3bbec02981ccaf42f7fee3af51
Ehsan <spin313@gmail.com>
2024-07-18 14:27:38 +0330

    Some unit tests have been added

--- tests/test_client.py
+++ tests/test_client.py
@@ -3,6 +3,8 @@ import httpx
 from httpx import Response, RequestError
 from cluster_client.client import ClusterClient
 from unittest import mock
+from cluster_client.config import HOSTS
+from cluster_client.exceptions import GroupOperationException
 
 
 @pytest.mark.asyncio
@@ -13,7 +15,7 @@ async def test_create_group_on_host_success(mocker):
     mock_response.status_code = 201
 
     mocker.patch.object(httpx.AsyncClient, 'post', return_value=mock_response)
-    result = await client._create_group_on_host(httpx.AsyncClient(), 'http://node1.example.com', 'test_group')
+    result = await client._create_group_on_host(httpx.AsyncClient(), HOSTS[0], 'test_group')
 
     assert result is True
 
@@ -26,7 +28,7 @@ async def test_create_group_on_host_failure_status_code(mocker):
     mock_response.status_code = 400
 
     mocker.patch.object(httpx.AsyncClient, 'post', return_value=mock_response)
-    result = await client._create_group_on_host(httpx.AsyncClient(), 'http://node1.example.com', 'test_group')
+    result = await client._create_group_on_host(httpx.AsyncClient(), HOSTS[0], 'test_group')
 
     assert result is False
 
@@ -36,6 +38,39 @@ async def test_create_group_on_host_request_error(mocker):
     client = ClusterClient()
 
     mocker.patch.object(httpx.AsyncClient, 'post', side_effect=RequestError('Request failed'))
-    result = await client._create_group_on_host(httpx.AsyncClient(), 'http://node1.example.com', 'test_group')
+    result = await client._create_group_on_host(httpx.AsyncClient(), HOSTS[0], 'test_group')
 
     assert result is False
+
+
+@pytest.mark.asyncio
+async def test_delete_group_on_host_success():
+    client = ClusterClient()
+
+    mock_response = mock.Mock(spec=Response)
+    mock_response.status_code = 200
+
+    with mock.patch.object(httpx.AsyncClient, 'request', return_value=mock_response):
+        result = await client._delete_group_on_host(httpx.AsyncClient(), HOSTS[0], 'test_group')
+        assert result is True
+
+
+@pytest.mark.asyncio
+async def test_delete_group_on_host_failure_status_code():
+    client = ClusterClient()
+
+    mock_response = mock.Mock(spec=Response)
+    mock_response.status_code = 400
+
+    with mock.patch.object(httpx.AsyncClient, 'request', return_value=mock_response):
+        result = await client._delete_group_on_host(httpx.AsyncClient(), HOSTS[0], 'test_group')
+        assert result is False
+
+
+@pytest.mark.asyncio
+async def test_delete_group_on_host_request_error():
+    client = ClusterClient()
+
+    with mock.patch.object(httpx.AsyncClient, 'request', side_effect=RequestError('Request failed')):
+        result = await client._delete_group_on_host(httpx.AsyncClient(), HOSTS[0], 'test_group')
+        assert result is False
