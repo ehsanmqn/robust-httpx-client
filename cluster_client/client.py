35708fa0602f6ed8599e6fa58347314fbb3f0b1f
Ehsan <spin313@gmail.com>
2024-07-18 12:21:56 +0330

    _create_group_on_host method has been implemented

--- cluster_client/client.py
+++ cluster_client/client.py
@@ -9,3 +9,22 @@ logger = logging.getLogger(__name__)
 class ClusterClient:
     def __init__(self, hosts=HOSTS):
         self.hosts = hosts
+
+    async def _create_group_on_host(self, client: httpx.AsyncClient, host: str, group_id: str) -> bool:
+        """
+        Create a group on a specific host.
+        """
+
+        url = f'{host}/v1/group/'
+        try:
+            response = await client.post(url, json={'groupId': group_id})
+            if response.status_code == 201:
+                logger.info(f'Group {group_id} created on {host}')
+                return True
+
+            logger.error(f'Failed to create group on {host}: {response.status_code}')
+
+        except httpx.RequestError as exc:
+            logger.error(f'Request error occurred: {exc}')
+
+        return False
